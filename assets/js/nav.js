86a443e573138abdaf28d1f29ee7741e0e137cde
emmy-chatchapong <suup532@gmail.com>
2026-09-14 19:02:04 +0700

    Add files via upload

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -11,7 +11,7 @@
 
   var TICKER_ITEMS = [
     { href: '#', text: 'NPT48 OFFICIAL SITE' },
-    { href: '#', text: 'NPT48 'Omoidaseru Koi wo Shiyou มารักกันให้เป็นความทรงจำ(Thai Ver.)' },
+    { href: '#', text: 'NPT48 Omoidaseru Koi wo Shiyou มารักกันให้เป็นความทรงจำ(Thai Ver.)' },
     { href: '#', text: 'NewPlaneT48' }
   ];
 
